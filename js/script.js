2c365178609d29d1e166d32e5047e8bf3a6b3b7d
nataliiakutsovol <44994817+nataliiakutsovol@users.noreply.github.com>
2019-01-08 19:44:42 +0200

    is

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-function myFunction() {
-   var element = document.querySelector(".modal");
-   element.classList.toggle("appointment-form");
-};
+var button = document.querySelector(".button-search-hotel");
+var popup = document.querySelector(".appointment-form");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var date = popup.querySelector("[name=date]");
+var number = popup.querySelector("[name=number]");
+
+button.addEventListener("click", function(evt) {
+   evt.preventDefault();
+   popup.classList.add("modal-show");
+});
+ button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-close");
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
